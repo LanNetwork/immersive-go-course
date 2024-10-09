14ecf072fa9f079a59b36923f65a415ffb788c48
Ian McGeoy <ianmcgeoy@gmail.com>
2024-10-09 12:24:26 -0700

    Move formatted printing to function printDirectory(). Add check for length of args passed in, with default behavior of printing cwd

--- projects/cli-files/go-ls/cmd/root.go
+++ projects/cli-files/go-ls/cmd/root.go
@@ -6,18 +6,27 @@ import (
 )
 
 func Execute() {
-	cwd, err := os.Getwd()
-	if err != nil {
-		fmt.Println("Error reading cwd: ", err)
-		return
-	}
-
-	files, err := os.ReadDir(cwd)
-	if err != nil {
-		fmt.Println("Error running os.ReadDir(): ", err)
+	args := os.Args
+	if len(args) == 1 {
+		cwd, err := os.Getwd()
+		if err != nil {
+			fmt.Println("Error reading cwd: ", err)
+			os.Exit(1)
+		}
+		printDirectory(cwd)
 	}
 
 	// fmt.Println("Current dir contents:", files)
+	os.Exit(0)
+}
+
+func printDirectory(directory string) {
+	files, err := os.ReadDir(directory)
+	if err != nil {
+		fmt.Println("Error running os.ReadDir(): ", err)
+		os.Exit(1)
+	}
+
 	for _, file := range files {
 		if file.IsDir() {
 			fmt.Printf("%s/\n", file.Name())
