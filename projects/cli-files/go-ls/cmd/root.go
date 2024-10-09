7b1c5c6d4bc7c0586106b6a68ec010d6405a4dd1
Ian McGeoy <ianmcgeoy@gmail.com>
2024-10-09 11:43:39 -0700

    Added basic functionality to go-ls in cwd

--- projects/cli-files/go-ls/cmd/root.go
+++ projects/cli-files/go-ls/cmd/root.go
@@ -1,5 +1,28 @@
 package cmd
 
-func Execute() {
+import (
+	"fmt"
+	"os"
+)
 
+func Execute() {
+	cwd, err := os.Getwd()
+	if err != nil {
+		fmt.Println("Error reading cwd: ", err)
+		return
+	}
+
+	files, err := os.ReadDir(cwd)
+	if err != nil {
+		fmt.Println("Error running os.ReadDir(): ", err)
+	}
+
+	// fmt.Println("Current dir contents:", files)
+	for _, file := range files {
+		if file.IsDir() {
+			fmt.Printf("%s/\n", file.Name())
+		} else {
+			fmt.Println(file.Name())
+		}
+	}
 }
